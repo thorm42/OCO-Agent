6e32688b5154c44451fb43fb47bd4e27a2980c79
Thorm42 <thorm42@gmail.com>
2026-09-14 13:02:21 +0200

    Implement timeout for password rotation command
    Added a timeout for the password update command to prevent indefinite hanging during execution.

--- oco_agent/windows/password_rotation.py
+++ oco_agent/windows/password_rotation.py
@@ -4,13 +4,20 @@ import subprocess
 
 from .. import base_password_rotation, logger
 
+# maximum time (seconds) to wait for the command before giving up rather than
+# risking the agent hanging indefinitely mid-rotation
+NET_USER_TIMEOUT = 30
+
 
 class PasswordRotation(base_password_rotation.BasePasswordRotation):
 
 	def updatePassword(self, username, newPassword, oldPassword=None):
 		# update password in local database
 		cmd = ['net', 'user', username, newPassword]
-		res = subprocess.run(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.DEVNULL)
+		try:
+			res = subprocess.run(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.DEVNULL, timeout=NET_USER_TIMEOUT)
+		except subprocess.TimeoutExpired:
+			raise Exception(' '.join(cmd)+' did not respond within '+str(NET_USER_TIMEOUT)+'s, aborting')
 		if res.returncode == 0:
 			logger('Changed password of user "'+username+'" locally')
 		else:
